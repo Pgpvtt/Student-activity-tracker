d020caedcec5167763361edb91ba7b6ec0ab1885
Pgpvtt <piyush.unhive@gmail.com>
2026-04-23 00:18:56 +0530

    Delete smart-student-tracker (1) directory

--- smart-student-tracker (1)/routes/dashboardRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDashboardData } from '../controllers/dashboardController.js';
-import { authMiddleware } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/', authMiddleware, getDashboardData);
-
-export default router;
